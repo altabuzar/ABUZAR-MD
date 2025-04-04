a9b68888b9e7c9d33a0d26916739b9f5a88b46f7
Popkid-254 <popkidtechs@gmail.com>
2025-04-04 21:11:53 +0300

    Update set.js

--- set.js
+++ set.js
@@ -9,8 +9,8 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "ABUZAR",
-    CAPTION : process.env.CAPTION || "ABUZAR-MD",
+    OWNER_NAME: process.env.OWNER_NAME || "POPKID",
+    CAPTION : process.env.CAPTION || "POPKID-XTECH",
     NUMERO_OWNER : process.env.NUMERO_OWNER || "POPKID",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_REACT : process.env.AUTO_REACT || 'yes',
